771534c5662dedb5e0741cb6bc163fd83c5f2003
ShivamChaube9969 <shivamkchaube@gmail.com>
2017-07-20 23:15:55 +0530

    20-07(1)

--- src/net/shivam/javacodes/day4/TimeDisplay.java
+++ src/net/shivam/javacodes/day4/TimeDisplay.java
@@ -1,18 +1,45 @@
 package net.shivam.javacodes.day4;
-
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
+
 
 public class TimeDisplay {
 	
 	
-public static void main(String[] args) {
+public static void main(String[] args) throws InterruptedException {
 		int[] Time = TimeInput.Time();
-		int h = Time[0];
-		int m = Time[1];
-		int s = Time[2];
+		ArrayList<Integer> Reset = new ArrayList();
+		Reset.add(0, 24);
+		Reset.add(1, 59);
+		Reset.add(2, 59);
 		System.out.print(", So the time is :\n");
-		System.out.println(h+":"+m+":"+s);
+		for(;;){
+		Thread.sleep(1000);
+		Time[2]++;
+		System.out.println(Time[0]+":"+Time[1]+":"+Time[2]);
+		if(Time[2] == Reset.indexOf(2)){
+			Time[2] = 0;
+			Time[1]++;
+			System.out.println(Time[0]+":"+Time[1]+":"+Time[2]);
+		}
+		if(Time[1] == Reset.indexOf(1) && Time[2] == Reset.indexOf(2)){
+			Time[0]++;
+			Time[1] = 0;
+			Time[2] = 0;
+			System.out.flush();
+			System.out.println(Time[0]+":"+Time[1]+":"+Time[2]);
+		}
+		if(Time[0] == Reset.indexOf(0)) {
+			Time[0] = 0;
+			Time[1] = 0;
+			Time[2] = 0;
+			System.out.flush();
+			System.out.println(Time[0]+":"+Time[1]+":"+Time[2]);
+		}
+		
+		
+		}
 //		Arrays.toString(TimeInput.Time()[0]);
 	}
 
